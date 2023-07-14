67ac2155b706fa66a92461c3f67e8a60f7d551e8
springnuance <binh.nguyen@aalto.fi>
2023-07-14 16:09:11 +0300

    fixing stoploss

--- modules/stoploss.py
+++ modules/stoploss.py
@@ -225,5 +225,5 @@ def stopFD_MOO(targetCurves, simCurves, geometries, yieldingIndices, deviationPe
     stopAllCurvesCheck = True
     for geometry in geometries:
         yieldingIndex = yieldingIndices[geometry]
-        stopAllCurvesCheck = stopAllCurvesCheck & stopFD_SOO(targetCurves[geometry]['force'][yieldingIndex:], simCurves[geometry]['force'][yieldingIndex:], deviationPercent)
+        stopAllCurvesCheck = stopAllCurvesCheck & stopFD_SOO(targetCurves[geometry]['force'], simCurves[geometry]['force'], yieldingIndex, deviationPercent)
     return stopAllCurvesCheck
